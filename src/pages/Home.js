c56136ea312803403d2095a4f28eea55ccbe30e4
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 22:37:10 +0530

    Added Next button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,45 @@ const Home = () => {
 	const hackerNewsAPI = "http://hn.algolia.com/api/v1/search?query=";
 	const [query, setQuery] = useState("");
 	const [news, setNews] = useState([]);
-	const [currentPage, setPage] = useState(null);
+	const [currentPage, setPage] = useState(0);
+	const getData = () => {};
 	const onSearch = (e) => {
 		e.preventDefault();
+		if (currentPage != 0) {
+			return;
+		}
 		console.log(query);
-		axios.get(`${hackerNewsAPI}${query}`).then((res) => {
-			setPage(res.data.page);
-			setNews(res.data.hits);
-		});
+		axios
+			.get(`${hackerNewsAPI}${query}&page=${currentPage}&hitsPerPage=50`)
+			.then((res) => {
+				setPage(res.data.page);
+				setNews(res.data.hits);
+			});
 		console.log(news);
 	};
+	const nextPage = (e) => {
+		e.preventDefault();
+		setPage(currentPage + 1);
+		setNews([]);
+		axios
+			.get(`${hackerNewsAPI}${query}&page=${currentPage}`)
+			.then((res) => {
+				setPage(res.data.page);
+				setNews(res.data.hits);
+			});
+	};
 	return (
-		<div>
-			Home
-			<form onSubmit={onSearch}>
-				<input
-					onChange={(e) => setQuery(e.target.value)}
-					type="text"
-				></input>
-				<button type="Submit">Submit</button>
-			</form>
+		<div className="container">
+			<h3>Search Hacker News</h3>
+			<div className="search">
+				<form onSubmit={onSearch}>
+					<input
+						onChange={(e) => setQuery(e.target.value)}
+						type="text"
+					></input>
+					<button type="Submit">Submit</button>
+				</form>
+			</div>
 			<div>
 				{news.map(({ author, title, objectID }, idx) => (
 					<ArticlePreview
@@ -35,6 +54,7 @@ const Home = () => {
 					/>
 				))}
 			</div>
+			<button onClick={nextPage}>NextPage</button>
 		</div>
 	);
 };
