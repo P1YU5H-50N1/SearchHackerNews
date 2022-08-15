7ac46b882ea03c1b3419abe406a9e80975a6fa06
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-15 22:37:58 +0530

    Made it a recursive component

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
+import Article from "../components/Article";
+import { Markup } from 'interweave';
+
 
 const PostDetail = () => {
-	let { id } = useParams();
+	const { id } = useParams();
+	const hackerNewsAPI = `http://hn.algolia.com/api/v1/items/${id}`;
+	const [article, setArticle] = useState([]);
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [response, setResponse] = useState([]);
+
+	useEffect(() => {
+		axios.get(hackerNewsAPI).then((res) => {
+			setArticle(res.data.children);
+			setTitle(res.data.title);
+			setAuthor(res.data.author)
+			setResponse(res.data);
+			// console.log(article);
+		});
+	}, []);
+
 	console.log(id);
-	return <div>post_detail</div>;
+	return (
+		<div>
+			<h1>{title}</h1>
+			<Article  article={response} margin={0}/>
+		</div>
+	);
 };
 
 export default PostDetail;
